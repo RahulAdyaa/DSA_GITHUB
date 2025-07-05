7db7f0862b958bb1b7fcb3e59a84723271ae18bd
Rahul Adya <radya_be22@thapar.edu>
2025-07-05 18:41:56 +0530

    Detect Loop in Linked LIst

---
+++ LinkedList/HasCycle.cpp
@@ -0,0 +1,26 @@
+/**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slowp=head;
+        ListNode* fastp=head;
+        while(fastp!=nullptr && fastp->next!=nullptr){
+            slowp=slowp->next;
+            fastp=fastp->next->next;
+            if(slowp==fastp)
+            {
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
+
