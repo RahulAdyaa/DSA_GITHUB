6c69e227197e35f04720c50925d84a4672d1f86b
Rahul Adya <radya_be22@thapar.edu>
2025-07-12 18:21:11 +0530

    added another appraoch

--- Recursion/is_subsequnce.cpp
+++ Recursion/is_subsequnce.cpp
@@ -1,6 +1,28 @@
 #include<iostream>
 using namespace std;
 
+// Time: O(2^n), Space: O(2^n)
+// void getSubseq(int idx, string current, string &t, vector<string>& ans) {
+//         if (idx == t.size()) {
+//             ans.push_back(current); // Store each complete subsequence
+//             return;
+//         }
+
+//         // Include current character
+//         getSubseq(idx + 1, current + t[idx], t, ans);
+
+//         // Exclude current character
+//         getSubseq(idx + 1, current, t, ans);
+//     }
+
+//     bool isSubsequence(string s, string t) {
+//         vector<string> ans;
+//         getSubseq(0, "", t, ans); // Generate all subsequences of t
+
+//         // Check if s is one of the generated subsequences
+//         return std::find(ans.begin(), ans.end(), s) != ans.end();
+//     }
+
 
 int main(){
     string s;
@@ -24,4 +46,6 @@ int main(){
     }
 
     return 0;
+
+    // Time: O(n), Space: O(1)
 }
